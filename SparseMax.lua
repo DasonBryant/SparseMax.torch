2304b7bc5846941b83fdc88612339f06ec73284b
Allan Zelener <azelener@twitter.com>
2016-08-17 17:48:56 -0400

    Rewrote SparseMax to use Eq 14 for more efficient gradient computation and added SparseMaxLoss and SparseMaxCriterion.

--- SparseMax.lua
+++ SparseMax.lua
@@ -1,130 +1,67 @@
 require 'nn'
 
--- Implementation attempt of "From Softmax to Sparsemax: A Sparse Model of Attention and Multi-Label Classification"
--- paper (André F. T. Martins, Ramón Fernandez Astudillo: http://arxiv.org/abs/1602.02068) in Torch
+-- Implementation of Sparsemax function from:
+-- "From Softmax to Sparsemax: A Sparse Model of Attention and Multi-Label Classification"
+-- André F. T. Martins, Ramón Fernandez Astudillo (http://arxiv.org/abs/1602.02068)
 
-local SparseMax, _ = torch.class('nn.SparseMax', 'nn.Module')
+local SparseMax, parent = torch.class('nn.SparseMax', 'nn.Module')
+
+function SparseMax:__init()
+  parent:__init(self)
+  self.taus = torch.Tensor()
+end
 
 function SparseMax:updateOutput(input)
+  local dim = 1
+  local inputDim = input:nDimension()
+  if inputDim == 2 or inputDim == 4 then
+    dim = 2
+  elseif input:nDimension() > 4 then
+    error('1D, 2D, 3D or 4D tensor expected')
+  end
+  local sizeDim = input:size()[dim]
 
-    local dim = nil
-    local inputDim = input:nDimension()
+  -- Sort input in descending order.
+  -- (NOTE: Can be replaced with linear time selection method described here:
+  --  http://stanford.edu/~jduchi/projects/DuchiShSiCh08.html)
+  local zs = torch.sort(input, dim, true)
 
-    --for compatibility with SoftMax
-    if inputDim == 1 then
-        dim = 1
-    elseif inputDim == 2 then
-        dim = 2
-    elseif inputDim == 3 then
-        dim = 1
-    elseif inputDim == 4 then
-        dim = 2
-    end
+  local range = torch.range(1, sizeDim):typeAs(input)
+  if dim == 2 then
+    range = range:view(1, sizeDim)
+  end
+  range = range:expandAs(zs)
 
-    local bigk = input:size(dim)
+  -- Determine sparsity of projection
+  local bound = 1 + torch.cmul(range, zs)
+  local cumsum_zs = torch.cumsum(zs, dim)
+  local is_gt = torch.gt(bound, cumsum_zs):typeAs(range)
+  local k = torch.max(torch.cmul(range, is_gt), dim)
 
-    -- Sort the input in descending order
-    -- First line of Algorithm 1
-    local zs = torch.sort(input, dim, true)
+  -- Compute threshold function
+  local zs_sparse = torch.cmul(is_gt, zs)
+  self.taus = torch.cdiv(torch.sum(zs_sparse, dim) - 1, k)
 
-    -- Prepare a repeated range tensor
-    local range = torch.range(1, bigk):typeAs(input)
-    local rangeViewMask = zs:size():fill(1)
-    rangeViewMask[dim] = -1
-    range = range:view(rangeViewMask):expandAs(zs)
-
-    -- Element-wise multiply repeated range tensor with sorted input
-    local left = torch.cmul(range, zs) + 1
-    local right = torch.cumsum(zs, dim)
-
-    local lt = torch.gt(left, right)
-
-    -- Second line of Algorithm 1
-    local k = torch.max(torch.cmul(range, lt:typeAs(range)), dim)
-
-    -- Third line of Algorithm 1, also given in Equation (4)
-    local threshold = torch.cdiv(torch.sum(torch.cmul(lt:typeAs(zs), zs), dim)-1, k)
-    self.output = torch.cmax(torch.zeros(input:size()):typeAs(input), input-threshold:expandAs(input))
-
-    return self.output
+  -- Sparsemax
+  self.output = torch.cmax(
+    torch.zeros(input:size()):typeAs(input),
+    input - self.taus:expandAs(input)
+  )
+  return self.output
 end
 
 function SparseMax:updateGradInput(input, gradOutput)
+  local dim = 1
+  local inputDim = input:nDimension()
+  if inputDim == 2 or inputDim == 4 then
+    dim = 2
+  elseif input:nDimension() > 4 then
+    error('1D, 2D, 3D or 4D tensor expected')
+  end
+  local sizeDim = input:size()[dim]
 
-    local dim = nil
-    local inputDim = input:nDimension()
-
-    --for compatibility with SoftMax
-    if inputDim == 1 then
-        dim = 1
-    elseif inputDim == 2 then
-        dim = 2
-    elseif inputDim == 3 then
-        dim = 1
-    elseif inputDim == 4 then
-        dim = 2
-    end
-
-    local S = torch.ne(self.output, 0):typeAs(self.output)
-
-    -- Equation (12)
-    local J = torch.diag(S) - torch.ger(S,S) / torch.sum(S)
-
-    -- TODO: implement gradient for > 1D tensors
-    -- TODO: Implement Equation (14) for nicer complexity
-
-    self.gradInput = J * gradOutput
-    return self.gradInput
-
-end
-
-function SparseMax:test(cuda)
-    local cuda = cuda or false
-    local dtype = nil
-
-    if cuda then
-        dtype = 'torch.CudaTensor'
-    else
-        dtype = 'torch.DoubleTensor'
-    end
-
-    local a1 = torch.rand(10):type(dtype)
-    local a2 = torch.rand(2,10):type(dtype)
-    local a3 = torch.rand(4,2,2):type(dtype)
-    local a4 = torch.rand(2,5,2,2):type(dtype)
-
-    local soft = nn.SoftMax():type(dtype)
-    local sm = nn.SparseMax()
-
-    print('======= 1D Test =======')
-    print('-> 1D Input')
-    print(a1)
-    print('-> SoftMax')
-    print(soft:forward(a1))
-    print('-> SparseMax')
-    print(sm:forward(a1))
-
-    print('\n\n======= 2D Test =======')
-    print('-> 2D Input')
-    print(a2)
-    print('-> SoftMax')
-    print(soft:forward(a2))
-    print('-> SparseMax')
-    print(sm:forward(a2))
-
-    print('\n\n======= 3D Test =======')
-    print('-> 3D Input')
-    print(a3)
-    print('-> SoftMax')
-    print(soft:forward(a3))
-    print('-> SparseMax')
-    print(sm:forward(a3))
-
-    print('\n\n======= 4D Test =======')
-    print('-> 4D Input')
-    print(a4)
-    print('-> SoftMax')
-    print(soft:forward(a4))
-    print('-> SparseMax')
-    print(sm:forward(a4))
+  local nonzeros = torch.ne(self.output, 0):typeAs(self.output)
+  self.gradInput = gradOutput - torch.sum(torch.cmul(gradOutput,nonzeros),dim):expandAs(gradOutput)
+  self.gradInput = torch.cmul(nonzeros, self.gradInput)
+  return self.gradInput
 end
